3431213a4164f1189b4415b503a7b568d834edd3
Flipp3rrr <flipp3rrr@protonmail.com>
2022-12-15 11:29:08 +0100

    Finish day 1

---
+++ day1-2.py
@@ -0,0 +1,16 @@
+f_input = open("day1-input.txt", "r").readlines()
+f_input = [x[:-1] for x in f_input]
+
+l_output = []
+
+i = 0
+for element in f_input:
+    if element != "":
+        i = i + int(element)
+
+    if element == "":
+        l_output.append(i)
+        i = 0
+
+l_output.sort()
+print(l_output[len(l_output) - 1] + l_output[len(l_output) - 2] + l_output[len(l_output) - 3])
